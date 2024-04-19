38750b91b48512183d0106003cd230d613926ff6
frederic smée <ssdfred@yahoo.fr>
2024-04-19 16:47:42 +0200

    mise ajour du controller

--- controllers/BiereController.js
+++ controllers/BiereController.js
@@ -1,54 +1,67 @@
-const { query } = require('express');
 const Biere = require('../models/Biere');
+const { validationResult } = require('express-validator');
 
 
 const controllerBiere = {};
 
 controllerBiere.getAll = (req, res) => {
     Biere.find()
-        .then((Biere) => {
-            res.json(Biere);
+        .then(biere => {
+            res.json(biere);
         })
-        .catch((err) => {
-            res.json(err);
+        
+        .catch(err => {
+            res.status(500).json({ message: "Erreur lors de la récupération des bières", error: err });
         });
     }
 
 controllerBiere.show = (req, res) => {
-    Biere.findByID(req.params.bar_id)
-        .then((biere) => {
+    Biere.findById(req.params.id_biere)
+        .then(biere => {
+            if (!biere) {
+                return res.status(404).json({ message: "Bière non trouvée" });
+            }
             res.json(biere);
         })
-        .catch((err) => {
-            res.json(err);
+        .catch(err => {
+            res.status(500).json({ message: "Erreur lors de la récupération des bières", error: err });
         });
 }
 controllerBiere.store = (req, res) => {
     Biere.create(req.body)
-        .then((result) => {
-            res.json(result);
+        .then(biere => {
+            res.status(201).json(biere);
         })
-        .catch((err) => {
-            res.json(err);
+        .catch(err => {
+            res.status(500).json({ message: "Erreur lors de la récupération des bières", error: err });
         });
 }
 controllerBiere.update = (req, res) => {
-    Biere.updateOne({_id: req.params.id}, req.body)
-        .then((result) => {
+    Biere.findById(req.params.id_biere)
+        .then(biere => {
+            if (!biere) {
+                return res.status(404).json({ message: "Bière non trouvée" });
+            }
+            return Biere.updateOne({_id: req.params.id_biere}, req.body);
+        })
+        .then(result => {
+            if (result.nModified === 0) {
+                return res.status(400).json({ message: "Aucun changement à apporter" });
+            }
             res.json(result);
         })
-        .catch((err) => {
-            res.json(err);
+        .catch(err => {
+            res.status(500).json({ message: "Erreur lors de la récupération des bières", error: err });
         });
 }
 
 controllerBiere.delete = (req, res) => {
-    Biere.findByIdAndDelete({_id: req.params.id}, req.body)
-        .then((result) => {
+    Biere.findByIdAndDelete(req.params.id_biere)
+        .then(result => {
             res.json(result);
         })
-        .catch((err) => {
-            res.json(err);
+        .catch(err => {
+            res.status(500).json({ message: "Erreur lors de la récupération des bières", error: err });
         });
     };
 module.exports = controllerBiere;
