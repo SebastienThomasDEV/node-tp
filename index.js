d976690cee8178075b99945c5738331c1367c138
Cherhazad <cherhazad.boustani@outlook.fr>
2024-04-19 16:24:33 +0200

    index update, routeur

--- index.js
+++ index.js
@@ -3,21 +3,33 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+
+// routeur
+const BarsRouter = require("./router/BarsRouter");
+const BiereRouter = require("./router/BiereRouter");
+const CommandeRouter = require("./router/CommandeRouter");
+
+
+
 // const cors = require('cors');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
-require('dotenv').config();
-mongoose.connect(process.env.MONGO_URI).then((res) => {
-    console.log('Connected to MongoDB');
-    app.listen(process.env.SERVER_PORT, () => {
-        console.log('Server is running on port 3000');
-    });
-    app.use('/commandes', require('./router/CommandeRouter'));
-}).catch((err) => {
-    console.log(err);
-    console.log("Not connected to MongoDB");
-});
 
-app.use('/biere', require('./router/BiereRouter'));
+mongoose.connect(process.env.MONGO_URI)
+    .then((res) => {
+        console.log('Connected to MongoDB');
+        app.listen(process.env.SERVER_PORT, () => {
+            console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+        });
+    }).catch((err) => {
+        console.log(err);
+        console.log("Not connected to MongoDB");
+    });
+
+app.use('/commandes', CommandeRouter);
+app.use('/biere', BiereRouter);
+app.use('/bars', BarsRouter);
+
 
