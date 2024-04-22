40ea704023ccad397aa3a89a15200e65199d52ea
frederic smée <ssdfred@yahoo.fr>
2024-04-22 10:04:45 +0200

    modification touter et controller

--- models/Biere.js
+++ models/Biere.js
@@ -1,31 +1,36 @@
-const mongoose = require('mongoose');
+
 /*Biere
   name: string
   description: text
   degree : float
   prix: float, min(0)
   bars_id: integer*/
-const biereSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    degree: {
-        type: Number,
-        required: true
-    },
-    prix: {
-        type: Number,
-        required: true
-    },
-    bar_id: {
-        type: Number,
-        required: true
-    }
-});
-module.exports = mongoose.model('biere', biereSchema);
+  const mongoose = require('mongoose');
+
+  const biereSchema = new mongoose.Schema({
+      name: {
+          type: String,
+          unique: true,
+         // required: true
+      },
+      description: {
+          type: String,
+          //required: true
+      },
+      degree: {
+          type: Number,
+          //required: true
+      },
+      prix: {
+          type: Number,
+          //required: true,
+          min: 0
+      },
+      bar_id: {
+          type: Number,
+          //required: true
+      }
+  });
+  
+  module.exports = mongoose.model('Biere', biereSchema); // Utilisation de 'Biere' pour le nom du modèle
+  
